4404e8350513e29a8d40e7affd1908c4695ecdfe
triki3melek-cmyk <triki3melek@gmail.com>
2026-01-01 15:36:33 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'rdv-v3';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
